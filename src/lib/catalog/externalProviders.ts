80fcb8ab0feafcf729fafdce67316a624f1986bd
Rishikeshsanin <141367936+Rishikeshsanin@users.noreply.github.com>
2026-09-16 01:03:44 +0530

    feat: include Vaanzari ethnic catalog feed

--- src/lib/catalog/externalProviders.ts
+++ src/lib/catalog/externalProviders.ts
@@ -3,11 +3,13 @@ import { mockShopNetworkProvider } from './providers/mockShopNetwork'
 import { openBeautyProvider } from './providers/openBeauty'
 import { sceneSkuProvider } from './providers/scenesku'
 import { soleScoutProvider } from './providers/soleScout'
+import { vaanzariProvider } from './providers/vaanzari'
 import type { ManagedProvider } from './providers/shared'
 
 export const externalCatalogProviders: ManagedProvider[] = [
   sceneSkuProvider,
   mockShopNetworkProvider,
+  vaanzariProvider,
   dummyDepartmentsProvider,
   soleScoutProvider,
   openBeautyProvider,
